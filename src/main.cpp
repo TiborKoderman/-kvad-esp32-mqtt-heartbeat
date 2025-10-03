cbee266d3fd848051494f5439e7a8f9f443feebe
Tibor Koderman <tibor.koderman@gmail.com>
2025-10-03 14:37:41 +0200

    feat: Update main.cpp to include necessary headers and initialize NVS flash

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,12 @@
-#include <time.h>
-#include "config.h"
-#include "main.hpp"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_netif.h"
 
+extern "C" void main(void)
+{
+    ESP_ERROR_CHECK(nvs_flash_init());
+}
